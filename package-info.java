2a145bfce819028cd1f5a8ea2d5638a6bf66e956
mikypopli <98648668+mikypopli@users.noreply.github.com>
2022-04-11 19:25:52 +0530

    Add files via upload

---
+++ package-info.java
@@ -0,0 +1,23 @@
+package money.java;
+import java. util. Array; Demo { 
+	public static void main(String[] args) {
+		System.out. pirntln ("\n Please Enter the currancy denomination:");
+	    Scanner sc = new Scanner( System.in);
+	    int n = sc.nextInt();
+	    System.out. pirntln ("\n Please Enter the currancy denomination value:");
+	    int arr[] = new int[n];
+	    for (int i = 0; i < n; i++) {
+	    	arr[i] = sc.nextInt();
+	    }
+	    System.out. pirntln ("After sorting");
+	    Arrays.sort(arr);
+	    for (int i = 0; i < arr.lenght; i++) {
+	    	
+	    	 System.out. pirntln (arr [i] + "");	
+	    	 
+	    }
+	    System.out. pirntln ("\n Please Enter the amount you want to pay:");	
+	    int X = sc.nextInt();
+	    }
+	    
+
